12c9cb97d630db27205a5c5bb7836d135587c56e
Shobhit27kulsh <shobhit09.sk@gmail.com>
2024-02-20 13:00:39 +0530

    updated

--- Q1.php
+++ Q1.php
@@ -1,1 +1,26 @@
-retiepritetriyp
+1.shopping create_function
+<?php
+function calculatetotalprice($items)
+{
+    $totalprice=0;
+    foreach($items as $item)
+    { 
+        $totalprice+=$item['price'];
+    }
+    if($totalprice > 5000)
+    {
+        $discountedprice=$totalprice * 0.9;
+        return $discountedprice;
+
+
+    }
+    else{
+        return $totalprice;
+
+
+    }
+}
+//for examaple-->
+$items=array(array("item"=>"shirt","price"=>1500),("item"=>"shirt","price"=>1500),("item"=>"shirt","price"=>1500));
+$totalprice=calculatetotalprice($item);
+echo "total price is Rs.".$totalprice;
