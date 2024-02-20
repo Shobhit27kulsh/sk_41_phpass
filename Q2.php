10b695b8162107bba6c7aa00dcd921f1689c1d9d
Shobhit27kulsh <shobhit09.sk@gmail.com>
2024-02-20 13:12:44 +0530

    updated

---
+++ Q2.php
@@ -0,0 +1,13 @@
+2.traffic light simulation
+<?php
+$color="red"; //let the color be red
+if($color=="green")
+{echo "green-go";}
+elseif($color=="yellow")
+{echo "yellow-slowdown";}
+elseif($color=="red";)
+{echo "red-stop";}
+else{
+    ech "invalid color";
+}
+?>
